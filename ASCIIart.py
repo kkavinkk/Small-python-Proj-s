3f87cc325bc50bfdcdcae059a14a2d3530c2daf9
kkavinkk <gk.kavin05@gmail.com>
2024-09-17 15:51:33 -0600

    deleted extra files

--- ASCIIart.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-image_path = r'ASCIIart\bluecar.jpg'
-with Image.open(image_path) as img:
-
-    width, height = img.size
-    print(f"Width: {width}")
-    print(f"Height: {height}")
