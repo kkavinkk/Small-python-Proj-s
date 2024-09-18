3ff284e0e88476fb44e9f1645ba9a695f3fbf1dd
kkavinkk <gk.kavin05@gmail.com>
2024-09-18 16:16:42 -0600

    update

--- ASCIIart/ASCIIart.py
+++ ASCIIart/ASCIIart.py
@@ -1,8 +1,16 @@
 from PIL import Image
+import numpy as np
 
-image_path = r'ASCIIart\bluecar.jpg'
+image_path = r'C:\Users\kavin\Downloads\code\Small-python-Proj-s\ASCIIart\bluecar.jpg'
 with Image.open(image_path) as img:
 
     width, height = img.size
     print(f"Width: {width}")
     print(f"Height: {height}")
+
+pixels = img.getdata(height, width)
+print(pixels)
+
+#for x in len(pixel_matrix):
+#    for y in len(pixel_matrix[x]):
+#        pixel = pixel_matrix[x][y]
