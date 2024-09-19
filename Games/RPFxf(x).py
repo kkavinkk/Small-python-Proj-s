3369317b51a5dc81f04381a0ef38bcc2a0af9dcc
kkavinkk <gk.kavin05@gmail.com>
2024-09-18 23:47:41 -0600

    done rps with fuinctions

--- Games/RPFxf(x).py
+++ Games/RPFxf(x).py
@@ -1,29 +1,29 @@
 import random
 
 def player():
-    choice = input("Rock, Paper or Scissors?:").lower
-    while choice not in ['Rock', 'Paper', 'Scissors']:
-        choice = input("Invalid choice, Rock, Paper or Scissors?:").lower()
+    choice = input("rock, paper or scissors?: ").lower()
+    while choice not in ['rock', 'paper', 'scissors']:
+        choice = input("Invalid choice, rock, paper or scissors?: ").lower()
     return choice
 
 def computer():
-    computerchoice = ['Rock', 'Paper', 'Scissors']
+    computerchoice = ['rock', 'paper', 'scissors']
     return random.choice(computerchoice)
 
-def main(choice, computerchoice):
-    if choice == computer:
-        print("Its a Tie")
+def winner(choice, computerchoice):
+    if choice == computerchoice:
+        return "Its a Tie" 
     elif(choice == 'rock' and computerchoice == "Scissors") or \
         (choice == 'rock' and computerchoice == "Scissors") or \
-        (choice == 'rock' and computerchoice == "Scissors") or :
-        print("You beat the mighty computer")
+        (choice == 'rock' and computerchoice == "Scissors"):
+        return "You beat the mighty computer"
     else:
         return "Computer Wins! We are all doomed"
     
-def play:
-    choice = getuserchoice()
-    computerchoice = getcomputerchoice()
+def play():
+    choice = player()
+    computerchoice = computer()
     result = winner(choice, computerchoice)
     print(result)
 
-play_game()
+play()
