11ee8eea54a00f6cfef56bace0c023925a3047bf
kkavinkk <gk.kavin05@gmail.com>
2024-09-19 09:28:55 -0600

    trying to change ASCII art to give rgb values

--- ASCIIart/ASCIIart.py
+++ ASCIIart/ASCIIart.py
@@ -1,16 +1,33 @@
 from PIL import Image
-import numpy as np
+
+
+def image_to_rgb(image_path):
+    #Retreive the Image
+    image = Image.open(image_path)
+
+    #We can convert to "RGB" mode
+    image = image.convert('RGB')
+
+    #find dimensions of the image
+    width, height = image.size
+
+    # retreive all pixel data
+    pixels = list(image.getdata())
+
+    # IMPORTANT** a 2d array is a LIST OF LISTS(List x List)
+    rgb_array = [] #starts with and empty list
+    for y in range(height):
+        row = [] #Empty list for top to bottom
+        for x in range(width):
+            #Calculate the dimensions of our array
+            index = y * width + x
+            row.append(pixels[index])
+        rgb_array.append(row)
+    
+    return rgb_array
 
 image_path = r'C:\Users\kavin\Downloads\code\Small-python-Proj-s\ASCIIart\bluecar.jpg'
-with Image.open(image_path) as img:
+rgb_array(image_path)
 
-    width, height = img.size
-    print(f"Width: {width}")
-    print(f"Height: {height}")
-
-pixels = img.getdata(height, width)
-print(pixels)
-
-#for x in len(pixel_matrix):
-#    for y in len(pixel_matrix[x]):
-#        pixel = pixel_matrix[x][y]
+for row in rgb_array:
+    print(row)
